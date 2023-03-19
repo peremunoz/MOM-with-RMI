72afe7191b1455bec771a21e407c9ef33e9c2c2e
peremunoz <peremunozfiguerol33@gmail.com>
2023-03-19 14:31:58 +0100

    MOM Server implementation
    Basic implementation of the server, with some messages for monitoring purposes. It can be executed with optional parameter [PORT] for binding the registry to an specific port

--- MOMServer.java
+++ MOMServer.java
@@ -1,2 +1,38 @@
-public class MOMServer {
+import exceptions.EMomError;
+
+import javax.sound.sampled.Port;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class MOMServer extends MOMServant {
+    public MOMServer() throws EMomError {}
+
+    private static int PORT = 1099;
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            PORT = Integer.parseInt(args[0]);
+        }
+
+        try {
+            System.out.println("Starting MOM Server...");
+            MOMServant mom = new MOMServant();
+            MOM momStub = (MOM) UnicastRemoteObject.exportObject(mom, 0);
+
+            // Create the JNDI registry
+            Registry registry = LocateRegistry.createRegistry(PORT);
+            System.out.println("JNDI registry created on port "+ PORT);
+
+            // Register the stub in the registry
+            registry.rebind("MOM", momStub);
+
+            System.out.println("MOM Server up!");
+
+        } catch (Exception e) {
+            System.out.println("Error starting the server: " + e.getMessage());
+        }
+    }
 }
