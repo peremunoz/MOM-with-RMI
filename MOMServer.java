7e2339147c8ca86be245ebe72789d20b56a2a0e1
peremunoz <peremunozfiguerol33@gmail.com>
2023-03-19 18:51:49 +0100

    Encapsulate the server initialization

--- MOMServer.java
+++ MOMServer.java
@@ -15,22 +15,26 @@ public class MOMServer extends MOMServant {
             PORT = Integer.parseInt(args[0]);
         }
 
+        System.out.println("Starting MOM Server...");
+
+        MsgQ_Init();
+
+        System.out.println("MOM Server up ✓");
+    }
+
+    private static void MsgQ_Init() {
         try {
-            System.out.println("Starting MOM Server...");
             MOMServant mom = new MOMServant();
             MOM momStub = (MOM) UnicastRemoteObject.exportObject(mom, 0);
 
             // Create the JNDI registry
             Registry registry = LocateRegistry.createRegistry(PORT);
-            System.out.println("JNDI registry created on port "+ PORT);
+            System.out.println("JNDI registry created on port "+ PORT + "✓");
 
             // Register the stub in the registry
             registry.rebind("MOM", momStub);
-
-            System.out.println("MOM Server up!");
-
         } catch (Exception e) {
-            System.out.println("Error in server > " + e.getMessage());
+            System.out.println("Error in server → " + e.getMessage());
         }
     }
 }
