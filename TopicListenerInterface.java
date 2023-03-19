8178b60ed3d97c1e8125f12d9c943d01443250f4
peremunoz <peremunozfiguerol33@gmail.com>
2023-03-19 19:52:49 +0100

    Add additional possible exception to be thrown

--- TopicListenerInterface.java
+++ TopicListenerInterface.java
@@ -1,4 +1,6 @@
+import exceptions.EMomError;
+
 public interface TopicListenerInterface extends java.rmi.Remote {
-    void onTopicMessage(String topicName, Message message) throws java.rmi.RemoteException;
+    void onTopicMessage(String topicName, Message message) throws java.rmi.RemoteException, EMomError;
     void onTopicClosed(String topicName) throws java.rmi.RemoteException;
 }
