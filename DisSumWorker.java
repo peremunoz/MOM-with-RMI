e3dae906b03eea1b4ee5c7bc28d2c6bd435aa045
Pere <peremunozfiguerol33@gmail.com>
2023-04-20 17:41:22 +0200

    Solve dumb error in isPrime function

--- DisSumWorker.java
+++ DisSumWorker.java
@@ -85,7 +85,7 @@ public class DisSumWorker implements TopicListenerInterface {
         if (num <= 1) {
             return false;
         }
-        for (int i = 2; num <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
